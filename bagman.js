f9725853c1b7c366eea1e1b6dcc3bafb7d906497
Matthias H. Risse <risse@risse.org>
2014-04-12 00:39:16 +0200

    Removing bloat

--- bagman.js
+++ bagman.js
@@ -1,10 +1,18 @@
-/*!
+/*
  * bagman.js
  * 
- * @url 		https://github.com/mhrisse/bagman
- * @author 		matthias@theotherpeople.de (@mhrisse)
- * @license 	MIT
- * @version 	0.2.2
+ * Find out which modules are on the page and lazy load and 
+ * evaluate your modular Javascript (AMD) accordingly. Handy for
+ * classic multi-page projects using requirejs. 
+ *
+ * - Tries to lazyload and lazyeval (via requirejs) all modules that are not in viewport on initial pageload
+ * - Populates the modules with some references, e.g. "el" or "config", etc.
+ *
+ * @url https://github.com/mhrisse/bagman
+ * @author risse@risse.org (@mhrisse)
+ * @license: MIT
+ * @version 0.2.3
+ *
  */
 
 define(
@@ -29,7 +37,8 @@ define(
 			loaded = 0,
 			priv = {},
 			pub = {},
-			helperId = 0;
+			helperId = 0,
+			uid = 0;
 
 		// set configuration (if any)
 		$.extend(config, module.config());
@@ -43,25 +52,35 @@ define(
 		pub.onloadDone = false;
 		pub.debug = config.debug;
 
+		/*
+		 * Publically accessible data
+		 */
+		pub.modules = [];
+		pub.instances = [];
+
+		/*
+		 * Init
+		 */
 		pub.init = function () {
 			// Mark modules which are inViewport
 			$.each(moduleQuery, function (index, value) {
-				if (priv.elementInViewport(value)) {
-					inViewport.push(value);
-					value.inViewport = true;
+				// ViewPort detection might not work in IE8 and below properly, so we exluce the old browsers for now.
+				if ($.browser.msie === true && $.browser.version < 9.0) {
 					priv.instanciate($(value).data(config.hook), value, $(value).data(config.config));
 				} else {
-					notInViewport.push(value);
-					value.inViewport = false;
+					if (priv.elementInViewport(value)) {
+						inViewport.push(value);
+						value.inViewport = true;
+						priv.instanciate($(value).data(config.hook), value, $(value).data(config.config));
+					} else {
+						notInViewport.push(value);
+						value.inViewport = false;
+					}
 				}
 			});
 
 			// Delay init of module not in viewport until onload has fired
             $(window).load(function () {
-				if (pub.debug) {
-					console.log('* onLoad ..');
-				}
-
 				pub.onloadDone = true;
 
 				$.each(notInViewport, function (index, value) {
@@ -72,56 +91,151 @@ define(
             return pub;
 		};
 
-		// private 
-		// http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+		/*
+		 * get module instance for given domNode
+		 * 
+		 * @param domNode object DomNode to search below
+		 * @return object Returns the corresponding module object or null.
+		 */
+		pub.getInstance = function (domNode) {
+			var id = $(domNode).attr('data-instance-id');
+			if(pub.instances[id]) {
+				return pub.instances[id];
+			} else {
+				return null;
+			}
+		};
+
+		/*
+		 * Get all modules instances of given domNode (that is modules nested inside the given DOM Node)
+		 * 
+		 * @param domNode object DomNode to search below
+		 * @param type string module to search for. This is basically filtering the result for a certain module type sich as sonyplayer
+		 * @return void
+		 */
+		pub.getInstances = function (domNode, type) {
+			// find all nested modules
+			var query = '*[data-module]',
+				nodes = [],
+				result = [];
+			
+			// filter for module type
+			if(type) {
+				query = '*[data-module="' + type + '"]';
+			}
+
+			nodes = $(domNode).find(query);
+
+			// loop thru them and transform data-instance-id into a true module reference
+			$.each(nodes, function (index, value) {
+				var instance = pub.instances[$(value).attr('data-instance-id')];
+				if(instance !== undefined) {
+					result.push(instance);
+				}
+			});
+
+			return result;
+		};
+
+		/*
+		 * Rescan the HTML and dispose all modules not longer on the page.
+		 * Important for PJAX, so we do not keep modules unnecessarily in memory.
+		 */
+		pub.update = function () {
+			// todo 
+			// uses delete()
+		};
+
+		/*
+		 * Remove a given module and all its data
+		 * in memory
+		 */
+		pub.remove = function (index) {
+			// todo 
+		};
+
+		/*
+		 * Very simple method to find out if the given DomNode is currently in viewport
+		 * 
+		 * @param el object DomNode
+		 * @url http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+		 * @return boolean
+		 */
 		priv.elementInViewport = function (el) {
 			var rect = el.getBoundingClientRect();
 			return rect.top < (window.innerHeight || document.body.clientHeight) && rect.left < (window.innerWidth || document.body.clientWidth);
 		};
 
+		/*
+		 * Instanciates a given module
+		 * 
+		 * @param module string
+		 * @param el object DomNode
+		 * @param config object configuration object
+		 * @return void
+		 */
 		priv.instanciate = function (module, el, config) {
 			helperId = helperId + 1;
 			var helper = 'helper' + (helperId),
 				required = [];
 
-			define(helper, [],
+			/*
+			 * For each instance, we are dynamically constructing 
+			 * a named helper module here, so we cann pass in a configuration 
+			 * object into the module we want to instanciate. 
+			 * There might be a simpler way to do this?
+			 */
+			define(
+				helper, 
+				[],
 				{
 					"module": module,
 					"el": el,
 					"config": config
 				}
-				);
+			);
 
-			// console.log(helper);
+			// put module and it's helper in an array
 			required.push(module);
 			required.push(helper);
 
 			require(required, function (mod, helper) {
-				var m = {};
 				try {
-					// intianitiate module and call it's init method, handing over the domnode
-					m = new mod.init($(helper.el), helper.config);
+					// creat instance (by cloning the module)
+					var uid = pub.instances.push($.extend(false, {}, mod));						
+					uid = uid - 1;
+					
+					// set relating domnode
+					pub.instances[uid].el = $(helper.el);
 
-					if (pub.debug) {
-						if (helper.el.inViewport === true) {
-							$(helper.el).css('border', '1px solid green');
-							console.log('init ..');
-							console.log($(helper.el));
-						} else {
-							$(helper.el).css('border', '1px solid orange');
-						}
+					// set module type
+					pub.instances[uid].module = helper.module;
+
+					// set instance id to domelement to allow backtracking  
+					pub.instances[uid].el.attr('data-instance-id', uid);
+					
+					// set instance id (just for easier debugging, 
+					// same as relating data-instance-id if you inspect el property)
+					pub.instances[uid].instanceId = uid;
+
+					// set relating config
+					pub.instances[uid].config = helper.config;
+
+					// if module has an init method, let's call it to kick it off
+					if(pub.instances[uid].init !== undefined) {
+						// execute the init function
+						pub.instances[uid].init($(helper.el), helper.config);
 					}
 
-					// console.dir(m);
 				} catch (e) {
 					if (pub.debug) {
-						console.log('module: :' + e.message);
+						console.log('module: ' + e.message);
 					}
 				}
 			});
 		};
 
-		// better be save than sorry
+		// domready, better be save than sorry
 		$(function () {
 
 			// start 
@@ -130,7 +244,6 @@ define(
 			// expose global bagman (for debug), if configured to do so
 			if (config.global === true) {
 				pub.config = config;
-				pub.modules = moduleQuery;
 				window.bagman = pub;
 			}
 
